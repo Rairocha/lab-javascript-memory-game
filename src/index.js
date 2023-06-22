89cf431b69fc48f6a02704c8296dbff6ce641666
Rairocha <52248306+Rairocha@users.noreply.github.com>
2023-06-22 20:19:38 -0300

    working with functions

--- src/index.js
+++ src/index.js
@@ -27,6 +27,95 @@ const cards = [
 
 const memoryGame = new MemoryGame(cards);
 
+
+function start(){
+  let html = '';
+  memoryGame.shuffleCards();
+  memoryGame.cards.forEach((pic) => {
+    html += `
+      <div class="card" data-card-name="${pic.name}">
+        <div class="back" name="${pic.img}"></div>
+        <div class="front" style="background: url(img/${pic.img}) no-repeat"></div>
+      </div>
+    `;
+  });
+
+  document.querySelector('#memory-board').innerHTML = html;
+
+};
+
+function blockCards(card){
+  if (memoryGame.pickedCards.length < 2) {
+    memoryGame.pickedCards.push(card)
+    card.classList.add('turned')
+  }
+
+  if (memoryGame.pickedCards.length === 2) {
+  let card1 = memoryGame.pickedCards[0]
+  let card2 = memoryGame.pickedCards[1]
+
+  let isPair = memoryGame.checkIfPair(card1.getAttribute('data-card-name'),
+   card2.getAttribute('data-card-name'))
+
+
+  if (isPair) {
+    card1.classList.add('blocked')
+    card1.classList.add('blocked')
+  } else {
+    setTimeout(() => {
+      card1.classList.toggle("turned")
+      card2.classList.toggle("turned")
+    }, 1500)
+    
+  }
+  memoryGame.pickedCards = []}
+
+};
+function updateScore(){
+  document.querySelector('#pairs-clicked').innerHTML=`${memoryGame.pairsClicked}`;
+  document.querySelector('#pairs-guessed').innerHTML=`${memoryGame.pairsGuessed}`;
+  if (memoryGame.checkIfFinished()){
+    setTimeout(() => {alert("Ganhou!")}, 250);
+    play();
+  }
+};
+function cardClicks(){
+  document.querySelectorAll('.card').forEach((card) => {
+  card.addEventListener('click', () => {
+    blockCards(card);
+    updateScore();
+  });
+});
+};
+function resetCards(){
+  document.querySelectorAll('.card').forEach(function(c){
+  c.classList.remove('turned');
+  c.classList.remove('blocked')});
+  memoryGame.pickedCards = [];
+  memoryGame.pairsClicked = 0;
+  memoryGame.pairsGuessed = 0;}
+;
+function play(){
+  resetCards();
+  start();
+  cardClicks();
+};
+window.addEventListener('load', (event) => {
+  play();
+});
+
+
+
+
+
+
+/*
+
+}
+
+}*/
+
+/*
 window.addEventListener('load', (event) => {
   let html = '';
   memoryGame.cards.forEach((pic) => {
@@ -42,10 +131,14 @@ window.addEventListener('load', (event) => {
   document.querySelector('#memory-board').innerHTML = html;
 
   // Bind the click event of each element to a function
+  
   document.querySelectorAll('.card').forEach((card) => {
     card.addEventListener('click', () => {
       // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
-    });
-  });
-});
+      
+      
+      
+    }
+    )
+  }
+  ); */
